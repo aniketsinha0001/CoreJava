77927efebf1d0b28c7789491a7b28280dad8c8ad
Aniket Sinha <37899215+aniketsinha0001@users.noreply.github.com>
2018-12-03 17:01:38 +0530

    Compare Two arrays Equal

---
+++ CheckWhetherTwoArraysEqual.java
@@ -0,0 +1,18 @@
+package com.sinha.aniket;
+
+import java.util.Arrays;
+
+public class CheckWhetherTwoArraysEqual {
+
+	public static void main(String[] args) {
+	
+		        int[] arr1 = new int [] {1, 2, 3, 4}; 
+		        int[] arr2 = new int [] {1, 2, 3, 4}; 
+		        int[] arr3 = new int [] {1, 2, 4, 3}; 
+		          
+		        System.out.println("is arr1 equals to arr2 : " + 
+		                                Arrays.equals(arr1, arr2)); 
+		        System.out.println("is arr1 equals to arr3 : " + 
+		                                Arrays.equals(arr1, arr3)); 
+		    } 
+		} 
